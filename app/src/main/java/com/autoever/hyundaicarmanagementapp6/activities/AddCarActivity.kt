108116ec071ec52120db787b4befd7eda1ed4bf2
junsoo1003 <royalex@dgu.ac.kr>
2024-12-05 16:53:27 +0900

    AddActivity

--- app/src/main/java/com/autoever/hyundaicarmanagementapp6/activities/AddCarActivity.kt
+++ app/src/main/java/com/autoever/hyundaicarmanagementapp6/activities/AddCarActivity.kt
@@ -1,6 +1,8 @@
 package com.autoever.hyundaicarmanagementapp6.activities
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,48 @@ import androidx.core.view.WindowInsetsCompat
 import com.autoever.hyundaicarmanagementapp6.R
 
 class AddCarActivity : AppCompatActivity() {
+    val carModels = listOf(
+        "Sonata",
+        "Grandeur",
+        "Elantra",
+        "Accent",
+        "Genesis",
+        "Tucson",
+        "Santa Fe",
+        "Palisade",
+        "Kona",
+        "Venue",
+        "Ioniq 5",
+        "Ioniq 6",
+        "Kona Electric",
+        "Ioniq Electric",
+        "Sonata Hybrid",
+        "Tucson Hybrid",
+        "Santa Fe Hybrid",
+        "Porter II",
+        "Starex",
+        "Staria"
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_add_car)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.spinner_item,
+            carModels
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
     }
 }
